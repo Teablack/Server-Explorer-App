499c01da89e916147a015761a262eb7581371e4e
teablack <ddaria.poda@gmail.com>
2025-09-08 19:19:13 +0200

    Add Vite plugins for ESLint and checker; update Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import checker from 'vite-plugin-checker';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => ({
+  plugins: [
+    react(),
+    mode !== 'test' &&
+      checker({
+        typescript: true,
+        overlay: {
+          initialIsOpen: false,
+        },
+        eslint: {
+          useFlatConfig: true,
+          lintCommand: 'eslint src',
+          dev: {
+            logLevel: ['warning', 'error'],
+          },
+        },
+      }),
+  ].filter(Boolean),
+}));

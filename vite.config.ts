7d3d1815474f1905b369df666048f48e78105a71
teablack <ddaria.poda@gmail.com>
2025-09-09 20:49:07 +0200

    Add vite-plugin-svgr for SVG support and update build script

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import checker from 'vite-plugin-checker';
+import svgrPlugin from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
+    svgrPlugin(),
     mode !== 'test' &&
       checker({
         typescript: true,
